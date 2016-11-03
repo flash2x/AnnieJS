477fa862e9f02287c8a33b2b1becc12b471083f3
anlun214 <anlun214@qq.com>
2016-11-03 11:58:58 +0800

    添加webgl渲染支持
    # Conflicts:
    #	README.md

---
+++ build/annieUI.d.ts
@@ -0,0 +1,451 @@
+/**
+ * Created by anlun on 16/8/14.
+ */
+/**
+ * @module annieUI
+ */
+declare namespace annieUI {
+    import Sprite = annie.Sprite;
+    /**
+     * 滚动视图，有些时候你的内容超过了一屏，需要上下或者左右滑动来查看内容，这个时候，你就应该用它了
+     * @class annieUI.ScrollPage
+     * @public
+     * @extends annie.Sprite
+     * @since 1.0.0
+     */
+    class ScrollPage extends Sprite {
+        /**
+         * 横向还是纵向 默认为纵向
+         * @property isVertical
+         * @type {boolean}
+         * @private
+         * @since 1.0.0
+         * @default true
+         */
+        private isVertical;
+        /**
+         * 可见区域的宽
+         * @property viewWidth
+         * @type {number}
+         * @private
+         * @since 1.0.0
+         * @default 0
+         */
+        private viewWidth;
+        /**
+         * 可见区域的高
+         * @property viewHeight
+         * @type {number}
+         * @private
+         * @since 1.0.0
+         * @default 0
+         */
+        private viewHeight;
+        /**
+         * 整个滚动的最大距离值
+         * @property maxDistance
+         * @type {number}
+         * @public
+         * @since 1.0.0
+         * @default 1040
+         */
+        maxDistance: number;
+        /**
+         * @property 滚动距离
+         * @type {number}
+         * @private
+         * @default 0
+         * @since 1.0.0
+         */
+        private distance;
+        /**
+         * 最小鼠标滑动距离
+         * @type {number}
+         */
+        private minDis;
+        /**
+         * 遮罩对象
+         * @property maskObj
+         * @since 1.0.0
+         * @private
+         * @type {annie.Shape}
+         */
+        private maskObj;
+        /**
+         * 真正的容器对象，所有滚动的内容都应该是添加到这个容器中
+         * @property view
+         * @public
+         * @since 1.0.0
+         * @type {annie.Sprite}
+         */
+        view: Sprite;
+        /**
+         * 最后鼠标经过的坐标值
+         * @property lastValue
+         * @private
+         * @since 1.0.0
+         * @type {number}
+         */
+        private lastValue;
+        /**
+         * 速度
+         * @property speed
+         * @private
+         * @since 1.0.0
+         * @type {number}
+         */
+        private speed;
+        /**
+         * 加速度
+         * @property addSpeed
+         * @private
+         * @since 1.0.0
+         * @type {number}
+         */
+        private addSpeed;
+        /**
+         * 是否是停止滚动状态
+         * @property isStop
+         * @public
+         * @since 1.0.0
+         * @type {boolean}
+         * @default true
+         */
+        isStop: boolean;
+        /**
+         * 滚动的最大速度，直接影响一次滑动之后最长可以滚多远
+         * @property maxSpeed
+         * @public
+         * @since 1.0.0
+         * @default 100
+         * @type {number}
+         */
+        maxSpeed: number;
+        /**
+         * 摩擦力,值越大，减速越快
+         * @property fSpeed
+         * @public
+         * @since 1.0.0
+         * @default 20
+         * @type {number}
+         */
+        fSpeed: number;
+        private paramXY;
+        private stopTimes;
+        private isMouseDown;
+        /**
+         * 是否是通过scrollTo方法在滑动中
+         * @property autoScroll
+         * @since 1.0.2
+         * @type {boolean}
+         * @private
+         * @default false;
+         */
+        private autoScroll;
+        /**
+         * 构造函数
+         * @method  ScrollPage
+         * @param {number}vW 可视区域宽
+         * @param {number}vH 可视区域高
+         * @param {number}maxDistance 最大滚动的长度
+         * @param {boolean}isVertical 是纵向还是横向，也就是说是滚x还是滚y,默认值为沿y方向滚动
+         */
+        constructor(vW: number, vH: number, maxDistance: number, isVertical?: boolean);
+        /**
+         * 改可滚动的方向，比如之前是纵向滚动的,你可以横向的。或者反过来
+         * @method changeDirection
+         * @param {boolean}isVertical 是纵向还是横向,不传值则默认为纵向
+         * @since 1.0.0
+         * @public
+         */
+        changeDirection(isVertical?: boolean): void;
+        /**
+         * 设置可见区域，可见区域的坐标始终在本地坐标中0,0点位置
+         * @method setMask
+         * @param {number}w 设置可见区域的宽
+         * @param {number}h 设置可见区域的高
+         * @public
+         * @since 1.0.0
+         */
+        private setMask(w, h);
+        private onMouseEvent(e);
+        /**
+         * 滚到指定的坐标位置
+         * @method
+         * @param dis 坐标位置
+         * @param time 滚动需要的时间
+         * @since 1.0.2
+         * @public
+         */
+        scrollTo(dis: number, time?: number): void;
+    }
+}
+/**
+ * Created by anlun on 16/8/14.
+ */
+/**
+ * @module annieUI
+ */
+declare namespace annieUI {
+    import Sprite = annie.Sprite;
+    /**
+     * 有时我们需要从外部获取一张个人头像，将它变成方形或者圆形展示出来。
+     * 又希望他能按照我们的尺寸展示，这个时候你就需要用到FacePhoto类啦。
+     * @class annieUI.FacePhoto
+     * @public
+     * @extends annie.Sprite
+     * @since 1.0.0
+    */
+    class FacePhoto extends Sprite {
+        constructor();
+        private photo;
+        private bitmap;
+        private maskType;
+        private radio;
+        private maskObj;
+        /**
+         * 被始化头像
+         * @method init
+         * @param src 头像的地址
+         * @param radio 指定头像的长宽
+         * @param maskType 遮罩类型，是圆形遮罩还是方形遮罩
+         */
+        init(src: string, radio?: number, maskType?: number): void;
+    }
+}
+/**
+ * Created by saron on 16/10/19.
+ */
+/**
+ * @module annieUI
+ */
+declare namespace annieUI {
+    import Sprite = annie.Sprite;
+    /**
+     * 滑动页面类
+     * @class annieUI.SlidePage
+     * @public
+     * @extends annie.Sprite
+     * @since 1.0.2
+     */
+    class SlidePage extends Sprite {
+        /**
+         * 页面个数
+         * @property listLen
+         * @type {number}
+         * @private
+         * @default 0
+         * @since 1.0.2
+         */
+        private listLen;
+        /**
+         * 页面滑动容器
+         * @property slideCon
+         * @type {annie.Sprite}
+         * @private
+         * @since 1.0.2
+         */
+        private slideCon;
+        /**
+         * 滑动完成回调函数
+         * @property callback
+         * @type {function}
+         * @since 1.0.2
+         * @private
+         */
+        private callback;
+        /**
+         * 滑动方向
+         * @property isVertical
+         * @type {boolean}
+         * @private
+         */
+        private isVertical;
+        /**
+         * 容器活动速度
+         * @property slideSpeed
+         * @type {number}
+         * @private
+         * @default 0
+         */
+        private slideSpeed;
+        /**
+         * 触摸点开始点X
+         * @property touchStartX
+         * @type {number}
+         * @private
+         */
+        private touchStartX;
+        /**
+         * 触摸点开始点Y
+         * @property touchStartY
+         * @type {number}
+         * @private
+         */
+        private touchStartY;
+        /**
+         * 触摸点结束点X
+         * @property touchEndX
+         * @type {number}
+         * @private
+         */
+        private touchEndX;
+        /**
+         * 触摸点结束点Y
+         * @property touchEndY
+         * @type {number}
+         * @private
+         */
+        private touchEndY;
+        /**
+         * 当前页面索引ID
+         * @property currentPageIndex
+         * @type {number}
+         * @since 1.0.2
+         * @public
+         */
+        currentPageIndex: number;
+        /**
+         * 页面是否移动
+         * @property isMoving
+         * @type {boolean}
+         * @since 1.0.2
+         * @public
+         */
+        isMoving: boolean;
+        /**
+         * 舞台宽
+         * @property stageW
+         * @type {number}
+         * @private
+         */
+        private stageW;
+        /**
+         * 舞台高
+         * @property stageH
+         * @type {number}
+         * @private
+         */
+        private stageH;
+        /**
+         * 页面列表
+         * @property pageList
+         * @type {Array}
+         * @private
+         */
+        private pageList;
+        /**
+         * 两点距离
+         * @property distance
+         * @type {number}
+         * @private
+         */
+        private distance;
+        /**
+         *
+         * @property fSpeed
+         * @type {number}
+         * @private
+         */
+        private fSpeed;
+        /**
+         * 是否点击了鼠标
+         * @property fSpeed
+         * @type {boolean}
+         * @private
+         */
+        private isMouseDown;
+        /**
+         * 是否可以下一页
+         * @property canSlideNext
+         * @type {boolean}
+         * @since 1.0.2
+         * @public
+         */
+        canSlideNext: boolean;
+        /**
+         * 是否可以上一页
+         * @property canSlidePrev
+         * @type {boolean}
+         * @since 1.0.2
+         * @public
+         */
+        canSlidePrev: boolean;
+        /**
+         * 手指滑动方向
+         * @property slideDirection
+         * @type {string}
+         * @public
+         * @since 1.0.2
+         */
+        slideDirection: string;
+        /**
+         * 是否为数组
+         * @param obj
+         * @returns {boolean}
+         * @private
+         */
+        private isArray;
+        /**
+         * 是否为函数
+         * @param fn
+         * @returns {boolean}
+         * @private
+         */
+        private isFunction(fn);
+        /**
+         * 构造函数
+         * @method SlidePage
+         * @public
+         * @since 1.0.2
+         * @param option      配置对象{pageList:pageList,callback:Fun,isVertical:true}
+         * @param{array}pageList     页面数组
+         * @param{method}callback    回调函数
+         * @param{boolean}isVertical 是纵向还是横向，也就是说是滚x还是滚y,默认值为沿y方向滚动
+         * @param{number}slideSpeed  页面滑动速度
+         * @example
+         *      var slideBox = new annieUI.SlidePage({
+         *          pageList: [new Page1(), new Page2(), new Page3(), new Page4()],//页面数组集
+         *          isVertical: true,//默认值为true,ture为纵向,falas为横向
+         *          slideSpeed: .32,//默认值为.4，滑动速度
+         *          callback:callback//滑动完成回调函数
+         *       });
+         */
+        constructor(option: any);
+        /**
+         * 添加到舞台
+         * @param e
+         */
+        private onAddToStage(e);
+        /**
+         * 触摸事件
+         * @param e
+         */
+        private onMouseEventHandler(e);
+        /**
+         * 滑动到指定页
+         * @method slideTo
+         * @param index 页面索引
+         * @public
+         * @since 1.0.2
+         */
+        slideTo(index: any): void;
+        /**
+         * 用于插入分页
+         * @method addPageList
+         * @public
+         * @since 1.0.2
+         * @param list 页面数组对象
+         */
+        addPageList(list: any): void;
+        /**
+         * 平面中两点距离公式
+         * @param x1
+         * @param y1
+         * @param x2
+         * @param y2
+         * @returns {number}
+         */
+        private getDistance(x1, y1, x2, y2);
+    }
+}
