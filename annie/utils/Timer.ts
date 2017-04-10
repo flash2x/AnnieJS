271a2ee0dfe7c4fc793009964b9b9f5bc2d1557c
anlun214 <anlun214@qq.com>
2017-04-10 17:57:15 +0800

    增加示例

--- annie/utils/Timer.ts
+++ annie/utils/Timer.ts
@@ -7,23 +7,23 @@ namespace annie {
      * @class annie.Timer
      * @public
      * @since 1.0.9
-     * @example
-     *      var timer=new annie.Timer(1000,10);
-     *      timer.addEventListener(annie.Event.TIMER,function (e) {
-     *          trace("once");
-     *      })
-     *      timer.addEventListener(annie.Event.TIMER_COMPLETE, function (e) {
-     *          trace("complete");
-     *          e.target.kill();
-     *      })
-     *      timer.start();
      */
     export class Timer extends annie.EventDispatcher{
         /**
          * 构造函数，初始化
          * @method Timer
-         * @param delay
-         * @param repeatCount
+         * @param {number} delay
+         * @param {number} repeatCount
+         * @example
+         *      var timer=new annie.Timer(1000,10);
+         *      timer.addEventListener(annie.Event.TIMER,function (e) {
+         *          trace("once");
+         *      })
+         *      timer.addEventListener(annie.Event.TIMER_COMPLETE, function (e) {
+         *          trace("complete");
+         *          e.target.kill();
+         *      })
+         *      timer.start();
          */
         constructor(delay:number, repeatCount:number = 0){
             super();
