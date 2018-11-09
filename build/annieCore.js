d44e1ea6623aff8314eb746450f542c3d3ac956e
anlun214 <anlun214@qq.com>
2018-11-10 00:24:39 +0800

    优化更新

--- build/annieCore.js
+++ build/annieCore.js
@@ -4153,7 +4153,7 @@ var annie;
         MovieClip.prototype.update = function (isDrawUpdate) {
             if (isDrawUpdate === void 0) { isDrawUpdate = true; }
             var s = this;
-            if (s._visible && isDrawUpdate && s._a2x_res_class.tf > 1) {
+            if (s._visible && s._a2x_res_class.tf > 1 && (isDrawUpdate || s._wantFrame)) {
                 if (s._mode >= 0) {
                     s._isPlaying = false;
                     s._curFrame = s.parent._curFrame - s._mode;
