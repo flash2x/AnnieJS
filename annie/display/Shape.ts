630cbaa7d4dd15707caf0aa56f7d0a9a4602540e
anlun214 <anlun214@qq.com>
2018-11-10 00:51:24 +0800

    优化更新

--- annie/display/Shape.ts
+++ annie/display/Shape.ts
@@ -513,7 +513,18 @@ namespace annie {
                 s._isBitmapStroke = null;
             }*/
         }
-
+        /**
+         * 设置虚线参数
+         * @method setLineDash
+         * @param {Array} data 一个长度为2的数组，第1个是虚线长度，第2个是虚线间隔，如果此参数为[]的空数组，则是清除虚线。
+         *                如[5,20]是画虚线,[]则是请除虚线，变为实线
+         * @since 2.0.2
+         * @return {void}
+         */
+        public  setLineDash(data:Array<number>=[]):void{
+            let c = this._command;
+            c[c.length] = [1, "setLineDash", [data,0]];
+        }
         /**
          * 解析一段路径 一般给Annie2x用
          * @method decodePath
--- build/annieCore.js
+++ build/annieCore.js
@@ -2984,6 +2984,19 @@ var annie;
                 s._isBitmapStroke = null;
             }*/
         };
+        /**
+         * 设置虚线参数
+         * @method setLineDash
+         * @param {Array} data 一个长度为2的数组，第1个是虚线长度，第2个是虚线间隔，如果此参数为[]的空数组，则是清除虚线。
+         *                如[5,20]是画虚线,[]则是请除虚线，变为实线
+         * @since 2.0.2
+         * @return {void}
+         */
+        Shape.prototype.setLineDash = function (data) {
+            if (data === void 0) { data = []; }
+            var c = this._command;
+            c[c.length] = [1, "setLineDash", [data, 0]];
+        };
         Shape.prototype.update = function (isDrawUpdate) {
             if (isDrawUpdate === void 0) { isDrawUpdate = false; }
             var s = this;
@@ -6978,7 +6991,7 @@ var annie;
      *      //打印当前引擎的版本号
      *      console.log(annie.version);
      */
-    annie.version = "2.0.1";
+    annie.version = "2.0.2";
     /**
      * 设备的retina值,简单点说就是几个像素表示设备上的一个点
      * @property annie.devicePixelRatio
