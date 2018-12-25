e38a1a0429bd92d53938f1a930ceb79e038764a1
anlun214 <anlun214@qq.com>
2018-12-25 16:10:18 +0800

    升级优化更新

--- annie/Annie.ts
+++ annie/Annie.ts
@@ -36,7 +36,7 @@ namespace annie {
      *      console.log(annie.version);
      */
 
-    export let version:string="2.0.2";
+    export let version:string="2.1.0";
 
     /**
      * <h4><font color="red">小游戏不支持 小程序不支持</font></h4>
