067d9c7a9ecd3b206f3fb12843ced4e8af9afe9c
anlun214 <anlun214@qq.com>
2019-01-08 15:56:38 +0800

    升级优化更新

--- build/annieCore.js
+++ build/annieCore.js
@@ -10630,3 +10630,7 @@ var annie;
 annie.Stage["addUpdateObj"](annie.Tween);
 annie.Stage["addUpdateObj"](annie.Timer);
 annie.Stage["flushAll"]();
+
+window.AnnieRoot= window;
+window.A2xExtend=__extends;
+window.trace=console.log;
