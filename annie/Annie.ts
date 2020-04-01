5ce8967262774960e698593cb8f305aa1ce93e75
anlun214 <anlun214@qq.com>
2020-04-01 22:27:30 +0800

    增加大尺寸资源是否分割渲染开关，annie.isCutDraw.默认关掉

--- annie/Annie.ts
+++ annie/Annie.ts
@@ -26,8 +26,12 @@ namespace annie {
      */
     export let debug: boolean = false;
     /**
+     * @property annie.isCutDraw
      * 是否对超大图像资源分割渲染
      * @type {boolean}
+     * @since 3.2.1
+     * @public
+     * @default false
      */
     export let isCutDraw: boolean = false;
     /**
