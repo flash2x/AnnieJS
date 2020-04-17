d8e25633145f77a918d58f32d4e66685b8be8aed
anlun214 <anlun214@qq.com>
2020-04-17 14:08:05 +0800

    修复影片剪辑作为遮罩时，遮罩缺失的bug

--- annie/display/Shape.ts
+++ annie/display/Shape.ts
@@ -4,8 +4,8 @@
 namespace annie {
     /**
      * 矢量对象
-     * @class annie.Bitmap
-     * @extends annie.DisplayObject
+     * @class annie.Shape
+     * @extends annie.Bitmap
      * @since 1.0.0
      * @public
      */
